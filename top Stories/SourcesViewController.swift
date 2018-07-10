bddb956d69f4ac28174fae0e6435f2e1bf93ba7b
Sanya Jain <sanyajain@sanyas-macbook-pro.local>
2018-07-10 11:23:55 -0500

    finished setting up the segways

--- top Stories/SourcesViewController.swift
+++ top Stories/SourcesViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class SourcesViewController: UITableViewController {
-
+    
     var sources = [[String: String]]()
     let apiKey = "5d892509a49046a087917c466fa80d09"
     
@@ -17,20 +17,22 @@ class SourcesViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "News Sources"
-        let query = "https://newsapi.org/v1/sources? language=en&country=us&apiKey=\(apiKey)"
-        if let url = URL(string: query) {
-            if let data = try? Data(contentsOf: url) {
-                let json = try! JSON (data: data)
-                if json["status"] == "ok" {
-                    parse(json: json)
-                    return
+        
+        let query = "https://newsapi.org/v1/sources?language=en&country=us&apiKey=\(apiKey)"
+        DispatchQueue.global(qos: .userInitiated).async {
+            [unowned self] in
+            if let url = URL(string: query) {
+                if let data = try? Data(contentsOf: url) {
+                    let json = try! JSON (data: data)
+                    if json["status"] == "ok" {
+                        self.parse(json: json)
+                        return
+                    }
                 }
             }
+            self.loadError()
         }
-        loadError()
         
-        
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     func parse(json: JSON) {
@@ -39,29 +41,61 @@ class SourcesViewController: UITableViewController {
             let name = result["name"].stringValue
             let description = result["description"].stringValue
             let source = ["id": id, "name": name, "description": description]
-             sources.append(source)
-            }
-        
-        tableView.reloadData()
+            sources.append(source)
+        }
         
         
+        
+        
+        DispatchQueue.main.async {
+            [unowned self] in
+            self.tableView.reloadData()
+        }
     }
     
     func loadError() {
-        let alert = UIAlertController(title: "Loading Error",
-                                      message: "There was a problem loading the news feed",
-                                      preferredStyle: .actionSheet)
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-        present(alert, animated: true, completion: nil) }
-    
-    
-    
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        DispatchQueue.main.async {
+            [unowned self] in
+            let alert = UIAlertController(title: "Loading Error",
+                                          message: "There was a problem loading the news feed",
+                                          preferredStyle: .actionSheet)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
     }
+        
+        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return sources.count
+        }
+        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+            let source = sources[indexPath.row]
+            cell.textLabel?.text = source["name"]
+            cell.detailTextLabel?.text = source["description"]
+            return cell
+            
+            
+        }
+        
+        
+        
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
+    
+    
+    @IBAction func ontappeddonebutton(_ sender: Any) {
+        exit(0)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let dvc = segue.destination as! ArticlesViewController
+        let index = tableView.indexPathForSelectedRow?.row
+        dvc.source = sources[index!]
+        dvc.apiKey = apiKey
 
-
+    }
+        
 }
 
