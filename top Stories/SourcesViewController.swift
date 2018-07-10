e974c8d20cc401bb2f3d46d512475ec94be6a0a5
Sanya Jain <sanyajain@sanyas-macbook-pro.local>
2018-07-10 10:20:17 -0500

    Set Up API protocols

--- top Stories/SourcesViewController.swift
+++ top Stories/SourcesViewController.swift
@@ -10,8 +10,16 @@ import UIKit
 
 class SourcesViewController: UITableViewController {
 
+    var sources = [[String: String]]()
+    let apiKey = "5d892509a49046a087917c466fa80d09"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "News Sources"
+        let query = "https://newsapi.org/v1/sources? language=en&country=us&apiKey=\(apiKey)"
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
